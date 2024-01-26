0e79110aaac98becfc5a7c9a9758a01514e1a4eb
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-01-26 22:23:04 +0300

    try fix

--- scripts/vk-messenger.sh
+++ scripts/vk-messenger.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-epm repack "https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/latest/vk-messenger.rpm"
+epm -y repack "https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/latest/vk-messenger.rpm"
